378e7c680457e789f98514a24582cacd31b92f98
Space Invader <amazing.space.invader@gmail.com>
2017-02-20 11:27:00 +0300

    Fin static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,37 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe 'Static Pages' do
-  describe 'Home Page' do
-    it 'should have the content \'Sample App\'' do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
+RSpec.feature 'Home Page', :type => :feature do
+  scenario 'should contain "Sample App"' do
+    visit '/static_pages/home'
+    expect(page).to have_text('Sample App')
+  end
+
+  scenario 'should have the title "Home"' do
+    visit '/static_pages/home'
+    expect(page).to have_title('Home')
+  end
+end
+
+RSpec.feature 'Help Page', :type => :feature do
+  scenario 'should contain "Help"' do
+    visit '/static_pages/help'
+    expect(page).to have_text('Help')
+  end
+
+  scenario 'should have the title "Help"' do
+    visit '/static_pages/help'
+    expect(page).to have_title('Help')
+  end
+end
+
+RSpec.feature 'About Page', :type => :feature do
+  scenario 'should contain "About"' do
+    visit '/static_pages/about'
+    expect(page).to have_text('About Us')
+  end
+
+  scenario '/static_pages/about' do
+    visit '/static_pages/about'
+    expect(page).to have_title('About Us')
   end
 end
